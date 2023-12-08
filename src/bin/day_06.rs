8395577b473d32b706843419d42960f21ae079d1
Ben Levin <bplevin36@gmail.com>
2023-12-08 15:41:46 +0000

    Add day 06 part 2; 17.29839ms

--- src/bin/day_06.rs
+++ src/bin/day_06.rs
@@ -1,18 +1,18 @@
 use std::time::Instant;
 
 use aoc2023::read_input;
-use nom::{sequence::tuple, character::complete::{u32 as parse_u32, multispace1, newline}, multi::separated_list1, bytes::complete::tag, IResult};
+use nom::{sequence::tuple, character::complete::{multispace1, newline, digit1}, multi::separated_list1, bytes::complete::tag, IResult};
 
 
-fn parse_input(input: &str) -> IResult<&str, (Vec<u32>, Vec<u32>)> {
+fn parse_input(input: &str) -> IResult<&str, (Vec<&str>, Vec<&str>)> {
     let (i, (_, _, times, _, _, _, distances)) = tuple((
         tag("Time:"),
         multispace1,
-        separated_list1(multispace1, parse_u32),
+        separated_list1(multispace1, digit1),
         newline,
         tag("Distance:"),
         multispace1,
-        separated_list1(multispace1, parse_u32),
+        separated_list1(multispace1, digit1),
     ))(input)?;
     Ok((i, (times, distances)))
 }
@@ -21,21 +21,20 @@ pub fn main() {
     let start_time = Instant::now();
     let input = read_input("06");
 
-    let (_, (times, records)) = parse_input(&input).unwrap();
+    let (_, (time_strs, record_strs)) = parse_input(&input).unwrap();
 
-    let mut product = 1;
-    for (time, record) in times.into_iter().zip(records.into_iter()) {
-        let mut num_winning = 0;
-        for hold_time in 1..time {
-            let time_to_race = time - hold_time;
-            let distance_traveled = time_to_race * hold_time;
-            if distance_traveled > record {
-                num_winning += 1;
-            }
+    let time = time_strs.join("").parse::<u64>().unwrap();
+    let record = record_strs.join("").parse::<u64>().unwrap();
+
+    let mut num_winning = 0;
+    for hold_time in 1..time {
+        let time_to_race = time - hold_time;
+        let distance_traveled = time_to_race * hold_time;
+        if distance_traveled > record {
+            num_winning += 1;
         }
-        product *= num_winning;
     }
 
     println!("Total time: {:?}", start_time.elapsed());
-    println!("{}", product);
+    println!("{}", num_winning);
 }
