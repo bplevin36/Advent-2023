ad40bdfd2cc4fa1adba27214f904f05dc9cafb7c
Ben Levin <bplevin36@gmail.com>
2023-12-08 00:35:25 +0000

    Add day 04 part 2 solution; 531.859µs

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, time::Instant};
+use std::{collections::{HashSet, HashMap}, time::Instant};
 
 use aoc2023::read_input;
 use nom::{
@@ -8,6 +8,7 @@ use nom::{
 
 
 struct Card {
+    id: u32,
     winning_numbers: HashSet<u32>,
     my_numbers: HashSet<u32>,
 }
@@ -27,18 +28,14 @@ impl Card {
             separated_list1(multispace1, parse_u32),
         ))(line)?;
         Ok((i, Card {
+            id,
             winning_numbers: winning_numbers.into_iter().collect(),
             my_numbers: my_numbers.into_iter().collect(),
         }))
     }
 
-    pub fn point_value(&self) -> u32 {
-        let num_winning = self.winning_numbers.intersection(&self.my_numbers).count() as u32;
-        if num_winning == 0 {
-            0
-        } else {
-            2u32.pow(num_winning - 1)
-        }
+    pub fn count_winning(&self) -> u32 {
+        self.winning_numbers.intersection(&self.my_numbers).count() as u32
     }
 }
 
@@ -46,11 +43,27 @@ pub fn main() {
     let start_time = Instant::now();
     let input = read_input("04");
 
-    let mut sum = 0;
+    // first parse all cards
+    let lines = input.lines();
+    let mut cards = Vec::with_capacity(lines.size_hint().0);
     for line in input.lines() {
         let (_, card) = Card::from_line(line).unwrap();
-        sum += card.point_value();
+        cards.push(card);
     }
+    // compute winning values in reverse order
+    let mut card_values = vec![0u32; cards.len()];
+    for i in (0..cards.len()).rev() {
+        let card = &cards[i];
+        let win_count = card.count_winning();
+        if win_count == 0 {
+            card_values[i] = 1;
+        } else {
+            let transitive_card_count: u32 = card_values[i + 1..i + 1 + win_count as usize].iter().copied().sum();
+            card_values[i] = transitive_card_count + 1;
+        }
+    }
+
+    let sum: u32 = card_values.iter().sum();
     println!("Total time: {:?}", start_time.elapsed());
     println!("{}", sum);
 }
