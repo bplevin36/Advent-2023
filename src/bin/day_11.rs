b2032320ac849ad0c057f3305d5ade917c7fecf1
Ben Levin <bplevin36@gmail.com>
2023-12-11 11:57:26 +0000

    Add day 11 part 2; 202.658µs

--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -6,23 +6,23 @@ pub fn main() {
     let start_time = Instant::now();
     let input = read_input("11");
 
+    const EXPANSION_FACTOR: usize = 999_999;
     let mut lines: Vec<Vec<u8>> = input.lines().map(|l| l.as_bytes().to_owned()).collect();
+    let mut empty_lines: Vec<usize> = Vec::new();
+    let mut empty_cols: Vec<usize> = Vec::new();
 
-    // expand empty lines
-    for i in (0..lines.len()).rev() {
+    // find empty lines
+    for i in 0..lines.len() {
         let line = &mut lines[i];
-        let line_len = line.len();
         if line.iter().all(|&b| b == b'.') {
-            lines.insert(i + 1, vec![b'.'; line_len]);
+            empty_lines.push(i);
         }
     }
 
-    // expand empty columns
-    for col_idx in (0..lines[0].len()).rev() {
+    // find empty columns
+    for col_idx in 0..lines[0].len() {
         if lines.iter().map(|l| l[col_idx]).all(|b| b == b'.') {
-            for line in lines.iter_mut() {
-                line.insert(col_idx + 1, b'.')
-            }
+            empty_cols.push(col_idx);
         }
     }
 
@@ -31,11 +31,20 @@ pub fn main() {
     for (line_idx, line) in lines.iter().enumerate() {
         for (col_idx, &byte) in line.iter().enumerate() {
             if byte == b'#' {
-                galaxies.push((line_idx, col_idx));
+                let mut galaxy_line = line_idx;
+                let mut galaxy_col = col_idx;
+                galaxy_line += match empty_lines.binary_search(&line_idx) {
+                    Ok(_num_empty) => panic!("This was supposed to be empty"),
+                    Err(num_empty) => num_empty * EXPANSION_FACTOR,
+                };
+                galaxy_col += match empty_cols.binary_search(&col_idx) {
+                    Ok(_num_empty) => panic!("This was supposed to be empty"),
+                    Err(num_empty) => num_empty * EXPANSION_FACTOR,
+                };
+                galaxies.push((galaxy_line, galaxy_col));
             }
         }
     }
-
     // compute distances
     let mut sum = 0;
     for i in 0..galaxies.len() - 1 {
