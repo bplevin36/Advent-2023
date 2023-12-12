daa4e22ff5b8153c4e747b17f1074af789121384
Ben Levin <bplevin36@gmail.com>
2023-12-12 22:10:47 +0000

    Day 12 part 2 adapted solution; 52.953483ms

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,146 +1,182 @@
 use std::{time::Instant, collections::HashMap};
 
 use aoc2023::read_input;
-use nom::{IResult, multi::{separated_list1, many1}, character::complete::{newline, multispace1, u32 as parse_u32, one_of}, sequence::separated_pair, bytes::complete::tag, combinator::map};
-
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
-#[repr(u8)]
-enum Spring {
-    Intact,
+#[derive(PartialEq, Eq, Clone, Copy, Hash)]
+enum SpringStatus {
+    Working,
     Broken,
     Unknown,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Row {
-    springs: Vec<Spring>,
-    runs: Vec<u8>,
+    springs: Vec<SpringStatus>,
+    blocks: Vec<usize>,
 }
 
-fn format_row(row: &Vec<Spring>) -> String {
-    let mut ret = String::new();
-    for spring in row.iter() {
-        ret.push(match spring {
-            Spring::Intact => '.',
-            Spring::Broken => '#',
-            Spring::Unknown => '?',
-        });
+fn parse_row(line: &str) -> Row {
+    let (s, b) = line.trim().split_once(' ').unwrap();
+
+    let springs: Vec<_> = s
+        .bytes()
+        .map(|c| match c {
+            b'.' => SpringStatus::Working,
+            b'#' => SpringStatus::Broken,
+            b'?' => SpringStatus::Unknown,
+            _ => unreachable!(),
+        })
+        .collect();
+
+    let blocks: Vec<_> = b.split(',').map(|s| s.parse::<usize>().unwrap()).collect();
+
+    // These are the biggest lengths that our hashing scheme can handle. It seems that
+    // the input doesn't include any larger values, but this is not guaranteed by
+    // the problem statement. In the worst case we'd need to switch these to usizes
+    // and just use a hashmap.
+    assert!(springs.len() <= 24);
+    assert!(blocks.len() <= 6);
+
+    Row { springs, blocks }
+}
+
+fn parse_input(input: &str) -> Vec<Row> {
+    input.lines().map(parse_row).collect()
+}
+
+type CacheKey<'c> = (&'c[SpringStatus], &'c[usize]);
+type Cache<'c> = HashMap<CacheKey<'c>, usize>;
+
+fn count_arrangements<'c>(row: &'c Row, cache: &mut Cache<'c>) -> usize {
+
+    fn cache_key<'c>(springs: &'c[SpringStatus], blocks: &'c[usize]) -> CacheKey<'c> {
+        (springs, blocks)
     }
-    ret
-}
 
+    fn get_cache(cache: &Cache, key: CacheKey) -> Option<usize> {
+        cache.get(&key).copied()
+    }
 
-type Cache<'c> = HashMap<(&'c[Spring], &'c[usize]), usize>;
+    fn set_cache<'c>(cache: &mut Cache<'c>, key: CacheKey<'c>, count: usize) -> usize {
+        cache.insert(key, count);
+        count
+    }
 
-fn parse_row(input: &str) -> IResult<&str, (Vec<Spring>, Vec<usize>)> {
-    let (i, (mut springs, mut runs)) = separated_pair(
-        many1(map(one_of(".#?"), |b| match b {
-            '.' => Spring::Intact,
-            '#' => Spring::Broken,
-            '?' => Spring::Unknown,
-            _ => panic!("Invalid"),
-        })),
-        multispace1,
-        separated_list1(tag(","), map(parse_u32, |n| n as usize)))(input)?;
+    fn munch_not_working(mut springs: &[SpringStatus], n: usize) -> Option<&[SpringStatus]> {
+        for _ in 0..n {
+            if let [SpringStatus::Unknown | SpringStatus::Broken, rest @ ..] = springs {
+                springs = rest;
+            } else {
+                return None;
+            }
+        }
 
-    // multiply everything by 5
-    let init_springs = springs.len();
-    springs.push(Spring::Unknown);
-    springs.extend_from_within(..);
-    springs.extend_from_within(..);
-    springs.extend_from_within(..init_springs);
-    runs = runs.repeat(5);
-
-    Ok((i, (springs, runs)))
-}
-
-
-fn attempt_leading_run(mut springs: &[Spring], run_len: usize) -> Option<&[Spring]> {
-    for i in 0..run_len {
-        if let Some(Spring::Broken | Spring::Unknown) = springs.get(i as usize) {
-            springs = &springs[1..];
+        if springs.first() == Some(&SpringStatus::Broken) {
+            None
+        } else {
+            Some(springs)
         }
     }
-    if let Some(Spring::Broken) = springs.first() {
-        None
-    } else {
-        Some(springs)
-    }
-}
 
-fn count_valid<'c>(springs_arg: &'c[Spring], runs: &'c[usize], cache: &mut Cache<'c>) -> usize {
-    let mut springs = springs_arg;
+    fn rec<'c>(mut springs: &'c[SpringStatus], blocks: &'c[usize], cache: &mut Cache<'c>) -> usize {
+        let key = cache_key(springs, blocks);
 
+        if let Some(count) = get_cache(cache, key) {
+            return count;
+        }
 
-    while let [Spring::Intact, rest @ ..] = springs {
-        springs = rest;
-    }
+        // strip leading working springs.
+        while let [SpringStatus::Working, rest @ ..] = springs {
+            springs = rest;
+        }
 
-    // If there are no springs, then there is only an arrangement if there are no blocks.
-    if springs.is_empty() {
-        return usize::from(runs.is_empty());
-    }
+        // If there are no springs, then there is only an arrangement if there are no blocks.
+        if springs.is_empty() {
+            return usize::from(blocks.is_empty());
+        }
 
-    // If there are no blocks, then there is only an arrangement if there are no broken springs.
-    if runs.is_empty() {
-        return usize::from(springs.iter().all(|s| *s != Spring::Broken));
-    }
+        // If there are no blocks, then there is only an arrangement if there are no broken springs.
+        if blocks.is_empty() {
+            return usize::from(springs.iter().all(|s| *s != SpringStatus::Broken));
+        }
 
-    let cache_key = (springs, runs);
-    if let Some(count) = cache.get(&cache_key) {
-        return *count;
-    }
+        // Easy case: if there are not enough springs to cover the blocks, then there are no arrangements.
+        if springs.len() < blocks.iter().sum::<usize>() + blocks.len() - 1 {
+            return set_cache(cache, key, 0);
+        }
 
-    // Easy case: if there are not enough springs to cover the blocks, then there are no arrangements.
-    if springs.len() < runs.iter().sum::<usize>() + runs.len() - 1 {
-        cache.insert(cache_key, 0);
-        return 0;
-    }
+        // If the first spring is unknown, then we can either assume it is working or broken, so we
+        // try both cases.
+        if springs[0] == SpringStatus::Unknown {
+            let count_if_working = rec(&springs[1..], blocks, cache);
 
-    if springs[0] == Spring::Unknown {
-        // recursively try both possibilities
-        let count_if_intact = count_valid(&springs[1..], runs, cache);
+            let count_if_broken = match munch_not_working(springs, blocks[0]) {
+                Some(munched) => rec(munched.get(1..).unwrap_or_default(), &blocks[1..], cache),
+                None => 0,
+            };
 
-        let count_if_broken = match attempt_leading_run(springs, runs[0]) {
-            Some(next_springs) => {
-                count_valid(&next_springs.get(1..).unwrap_or_default(), &runs[1..], cache)
-            },
+            return set_cache(cache, key, count_if_working + count_if_broken);
+        }
+
+        // Now it must be that springs[0] == SpringStatus::Broken.
+
+        let ret = match munch_not_working(springs, blocks[0]) {
+            Some(munched) => rec(munched.get(1..).unwrap_or_default(), &blocks[1..], cache),
             None => 0,
         };
-        let count = count_if_intact + count_if_broken;
-        cache.insert(cache_key, count);
-        return count;
+        set_cache(cache, key, ret)
     }
 
-    let count = match attempt_leading_run(springs, runs[0]) {
-        Some(next_springs) => {
-            count_valid(&next_springs.get(1..).unwrap_or_default(), &runs[1..], cache)
-        },
-        None => 0,
-    };
-    cache.insert(cache_key, count);
-    count
+    rec(&row.springs, &row.blocks, cache)
 }
 
-fn parse_rows(input: &str) -> IResult<&str, Vec<(Vec<Spring>, Vec<usize>)>> {
-    separated_list1(
-        newline, parse_row)(input)
+pub fn part1(input: &str) -> String {
+    let rows = parse_input(input);
+    let mut cache: Cache = HashMap::new();
+    rows.iter()
+        .map(|row| {
+            cache.clear();
+            count_arrangements(row, &mut cache)
+        })
+        .sum::<usize>()
+        .to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let mut rows = parse_input(input);
+    for row in &mut rows {
+        let n = row.springs.len();
+        row.springs.push(SpringStatus::Unknown);
+        row.springs.extend_from_within(..);
+        row.springs.extend_from_within(..);
+        row.springs.extend_from_within(..n);
+        row.blocks = row.blocks.repeat(5);
+    }
+    let mut cache: Cache = HashMap::new();
+    rows.iter()
+        .map(|row| {
+            cache.clear();
+            count_arrangements(row, &mut cache)
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 pub fn main() {
     let start_time = Instant::now();
-    let input = read_input("12_test");
+    let input = read_input("12");
 
+    println!("{}", part2(&input));
+    /*
     let (_i, rows) = parse_rows(&input).unwrap();
 
     let mut cache: Cache = HashMap::new();
     let mut sum = 0;
     for (springs, runs) in rows.iter() {
+        cache.clear();
         let count = count_valid(&springs, &runs, &mut cache);
         println!("Row {} count {}", format_row(springs), count);
         sum += count;
     }
-    println!("Total time: {:?}", start_time.elapsed());
     println!("{}", sum);
+    */
+    println!("Total time: {:?}", start_time.elapsed());
 }
