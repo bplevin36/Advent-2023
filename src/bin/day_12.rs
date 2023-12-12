ab408fc9e2358f73ab77c7b683c205d2904fd70f
Ben Levin <bplevin36@gmail.com>
2023-12-12 20:17:24 +0000

    WIP day 12 part 2

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,10 +1,10 @@
-use std::{fmt::Display, time::Instant};
+use std::{time::Instant, collections::HashMap};
 
 use aoc2023::read_input;
-use nom::{IResult, multi::{separated_list1, many1}, character::complete::{newline, multispace1, u8 as parse_u8, one_of}, sequence::separated_pair, bytes::complete::tag, combinator::map};
+use nom::{IResult, multi::{separated_list1, many1}, character::complete::{newline, multispace1, u32 as parse_u32, one_of}, sequence::separated_pair, bytes::complete::tag, combinator::map};
 
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 #[repr(u8)]
 enum Spring {
     Intact,
@@ -12,116 +12,134 @@ enum Spring {
     Unknown,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Row {
     springs: Vec<Spring>,
     runs: Vec<u8>,
 }
 
-impl Display for Row {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for spring in self.springs.iter() {
-            write!(f, "{}", match spring {
-                Spring::Intact => '.',
-                Spring::Broken => '#',
-                Spring::Unknown => '?',
-            })?;
+fn format_row(row: &Vec<Spring>) -> String {
+    let mut ret = String::new();
+    for spring in row.iter() {
+        ret.push(match spring {
+            Spring::Intact => '.',
+            Spring::Broken => '#',
+            Spring::Unknown => '?',
+        });
+    }
+    ret
+}
+
+
+type Cache<'c> = HashMap<(&'c[Spring], &'c[usize]), usize>;
+
+fn parse_row(input: &str) -> IResult<&str, (Vec<Spring>, Vec<usize>)> {
+    let (i, (mut springs, mut runs)) = separated_pair(
+        many1(map(one_of(".#?"), |b| match b {
+            '.' => Spring::Intact,
+            '#' => Spring::Broken,
+            '?' => Spring::Unknown,
+            _ => panic!("Invalid"),
+        })),
+        multispace1,
+        separated_list1(tag(","), map(parse_u32, |n| n as usize)))(input)?;
+
+    // multiply everything by 5
+    let init_springs = springs.len();
+    springs.push(Spring::Unknown);
+    springs.extend_from_within(..);
+    springs.extend_from_within(..);
+    springs.extend_from_within(..init_springs);
+    runs = runs.repeat(5);
+
+    Ok((i, (springs, runs)))
+}
+
+
+fn attempt_leading_run(mut springs: &[Spring], run_len: usize) -> Option<&[Spring]> {
+    for i in 0..run_len {
+        if let Some(Spring::Broken | Spring::Unknown) = springs.get(i as usize) {
+            springs = &springs[1..];
         }
-        Ok(())
+    }
+    if let Some(Spring::Broken) = springs.first() {
+        None
+    } else {
+        Some(springs)
     }
 }
 
-impl Row {
-    fn parse(input: &str) -> IResult<&str, Row> {
-        let (i, (springs, runs)) = separated_pair(
-            many1(map(one_of(".#?"), |b| match b {
-                '.' => Spring::Intact,
-                '#' => Spring::Broken,
-                '?' => Spring::Unknown,
-                _ => panic!("Invalid"),
-            })),
-            multispace1,
-            separated_list1(tag(","), parse_u8))(input)?;
-        Ok((i, Row{
-            springs,
-            runs
-        }))
+fn count_valid<'c>(springs_arg: &'c[Spring], runs: &'c[usize], cache: &mut Cache<'c>) -> usize {
+    let mut springs = springs_arg;
+
+
+    while let [Spring::Intact, rest @ ..] = springs {
+        springs = rest;
     }
 
-    fn all_possible(&self) -> Vec<Row> {
-        let mut possibles = Vec::new();
-        possibles.push(self.clone());
-
-        for i in 0..self.springs.len() {
-            if self.springs[i] == Spring::Unknown {
-                let mut clone_broken = possibles.clone();
-                for possible in possibles.iter_mut() {
-                    possible.springs[i] = Spring::Intact;
-                }
-                for possible in clone_broken.iter_mut() {
-                    possible.springs[i] = Spring::Broken;
-                }
-                possibles.append(&mut clone_broken);
-            }
-        }
-        possibles
+    // If there are no springs, then there is only an arrangement if there are no blocks.
+    if springs.is_empty() {
+        return usize::from(runs.is_empty());
     }
 
-    fn valid(&self) -> bool {
-        let mut run_count = 0;
-        let mut run_size = 0;
-        for s in self.springs.iter() {
-            match s {
-                Spring::Intact => {
-                    if run_size > 0 {
-                        if run_count >= self.runs.len() {
-                            return false;
-                        }
-                        if run_size != self.runs[run_count] {
-                            return false;
-                        }
-                        run_count += 1;
-                        run_size = 0;
-                    }
-                },
-                Spring::Broken => {
-                    run_size += 1;
-                },
-                Spring::Unknown => panic!("Invalid"),
-            }
-
-        }
-        if run_size > 0 {
-            if run_count >= self.runs.len() {
-                return false;
-            }
-            if run_size != self.runs[run_count] {
-                return false;
-            }
-            run_count += 1;
-        }
-        run_count == self.runs.len()
+    // If there are no blocks, then there is only an arrangement if there are no broken springs.
+    if runs.is_empty() {
+        return usize::from(springs.iter().all(|s| *s != Spring::Broken));
     }
+
+    let cache_key = (springs, runs);
+    if let Some(count) = cache.get(&cache_key) {
+        return *count;
+    }
+
+    // Easy case: if there are not enough springs to cover the blocks, then there are no arrangements.
+    if springs.len() < runs.iter().sum::<usize>() + runs.len() - 1 {
+        cache.insert(cache_key, 0);
+        return 0;
+    }
+
+    if springs[0] == Spring::Unknown {
+        // recursively try both possibilities
+        let count_if_intact = count_valid(&springs[1..], runs, cache);
+
+        let count_if_broken = match attempt_leading_run(springs, runs[0]) {
+            Some(next_springs) => {
+                count_valid(&next_springs.get(1..).unwrap_or_default(), &runs[1..], cache)
+            },
+            None => 0,
+        };
+        let count = count_if_intact + count_if_broken;
+        cache.insert(cache_key, count);
+        return count;
+    }
+
+    let count = match attempt_leading_run(springs, runs[0]) {
+        Some(next_springs) => {
+            count_valid(&next_springs.get(1..).unwrap_or_default(), &runs[1..], cache)
+        },
+        None => 0,
+    };
+    cache.insert(cache_key, count);
+    count
 }
 
-fn parse_rows(input: &str) -> IResult<&str, Vec<Row>> {
+fn parse_rows(input: &str) -> IResult<&str, Vec<(Vec<Spring>, Vec<usize>)>> {
     separated_list1(
-        newline, Row::parse)(input)
+        newline, parse_row)(input)
 }
 
 pub fn main() {
     let start_time = Instant::now();
-    let input = read_input("12");
+    let input = read_input("12_test");
 
     let (_i, rows) = parse_rows(&input).unwrap();
+
+    let mut cache: Cache = HashMap::new();
     let mut sum = 0;
-    for row in rows.iter() {
-        let possibles = row.all_possible();
-        for possible_row in possibles {
-            if possible_row.valid() {
-                sum += 1;
-            }
-        }
+    for (springs, runs) in rows.iter() {
+        let count = count_valid(&springs, &runs, &mut cache);
+        println!("Row {} count {}", format_row(springs), count);
+        sum += count;
     }
     println!("Total time: {:?}", start_time.elapsed());
     println!("{}", sum);
