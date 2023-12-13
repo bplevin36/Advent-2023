9706bcc0be0746333d5cf8c88eba894f98a0d92a
Ben Levin <bplevin36@gmail.com>
2023-12-13 14:05:17 +0000

    Add day 13 part 2; 124.443µs

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -20,8 +20,11 @@ pub fn main() {
 
     let mut sum = 0;
     for pattern in patterns.iter() {
-        match (find_horizontal_reflection(pattern), find_vertical_reflection(pattern)) {
-            (None, None) | (Some(_), Some(_)) => panic!("Invalid"),
+        match (find_horizontal_reflection_smudged(pattern), find_vertical_reflection_smudged(pattern)) {
+            (None, None) => panic!("no reflection"),
+            (Some(horiz), Some(vert)) => {
+                panic!("Both reflections: {}, {}", horiz, vert);
+            },
             (Some(horiz), None) => {
                 sum += 100 * horiz;
             },
@@ -34,31 +37,54 @@ pub fn main() {
     println!("Total time: {:?}", start_time.elapsed());
 }
 
-fn find_vertical_reflection(pattern: &[&[u8]]) -> Option<usize> {
+fn find_vertical_reflection_smudged(pattern: &[&[u8]]) -> Option<usize> {
     for col in 1..pattern[0].len() {
+        let mut smudged = false;
         let mut reflection_refuted = false;
         for line in pattern {
-            if !line[col..].iter().zip(line[..col].iter().rev()).all(|(b, a)| b == a) {
-                reflection_refuted = true;
+            for (byte_before, byte_after) in line[col..].iter().zip(line[..col].iter().rev()) {
+                if byte_before != byte_after {
+                    if !smudged {
+                        smudged = true;
+                    } else {
+                        reflection_refuted = true;
+                        break;
+                    }
+                }
+            }
+            if reflection_refuted {
                 break;
             }
         }
-
-        if !reflection_refuted {
+        if !reflection_refuted && smudged {
             return Some(col)
         }
     }
     None
 }
 
-fn find_horizontal_reflection(pattern: &[&[u8]]) -> Option<usize> {
+fn find_horizontal_reflection_smudged(pattern: &[&[u8]]) -> Option<usize> {
     for row in 1..pattern.len() {
+        let mut smudged = false;
+        let mut reflection_refuted = false;
         let rows_before = &pattern[..row];
         let rows_after = &pattern[row..];
-        let mirrored = rows_after.iter()
-            .zip(rows_before.iter().rev())
-            .all(|(ra, rb)| ra == rb);
-        if mirrored {
+        for (&row_after, row_before) in rows_after.iter().zip(rows_before.iter().rev()) {
+            for (byte_after, byte_before) in row_after.iter().zip(row_before.iter()) {
+                if byte_before != byte_after {
+                    if !smudged {
+                        smudged = true;
+                    } else {
+                        reflection_refuted = true;
+                        break;
+                    }
+                }
+            }
+            if reflection_refuted {
+                break;
+            }
+        }
+        if !reflection_refuted && smudged {
             return Some(row);
         }
     }
