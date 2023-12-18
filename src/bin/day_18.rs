00767c216dc8c17afcc06cb9ea75708025ed61d6
Ben Levin <bplevin36@gmail.com>
2023-12-18 18:07:48 +0000

    Add day 18 part 2; 117.164µs

--- src/bin/day_18.rs
+++ src/bin/day_18.rs
@@ -1,6 +1,12 @@
-use std::{time::Instant, ops::Neg};
+use std::time::Instant;
 
 use aoc2023::read_input;
+
+fn shoelace(coord1: (isize, isize), coord2: (isize, isize)) -> isize {
+    let (x1, y1) = coord1;
+    let (x2, y2) = coord2;
+    x1 * y2 - y1 * x2
+}
 
 pub fn main() {
     let start_time = Instant::now();
@@ -8,105 +14,51 @@ pub fn main() {
 
     let instructions = input.lines().map(|line| {
         let mut fields = line.split(" ");
-        let direction = fields.next().unwrap();
-        let distance = fields.next().unwrap().parse::<isize>().unwrap();
-        let color_field = fields.next().unwrap();
-        let color = &color_field[1..color_field.len() - 2];
-        (direction, distance, color)
-    }).collect::<Vec<(&str, isize, &str)>>();
+        let _color = fields.next().unwrap();
+        let _ = fields.next().unwrap().parse::<isize>().unwrap();
+        let mut color_field = fields.next().unwrap();
+        color_field = &color_field[2..color_field.len() - 1];
+        let distance = match isize::from_str_radix(&color_field[0..5], 16) {
+            Ok(d) => d,
+            Err(_) => panic!("Could not parse {}", &color_field[0..5]),
+        };
+        let direction = match color_field.as_bytes()[5] {
+            b'0' => "R",
+            b'1' => "D",
+            b'2' => "L",
+            b'3' => "U",
+            _ => panic!("Invalid"),
+        };
+        (direction, distance)
+    }).collect::<Vec<(&str, isize)>>();
 
-    // find how big our grid needs to be
     let (mut row, mut col) = (0isize, 0isize);
-    let (mut max_row, mut min_row, mut max_col, mut min_col) = (0, 0, 0, 0);
-    for &(direction, distance, _) in instructions.iter() {
+
+    let mut double_area = 0isize;
+
+    for i in 0..instructions.len() {
+        let &(direction, distance) = &instructions[i];
+        let (prev_row, prev_col) = (row, col);
         match direction {
             "U" => {
                 row -= distance;
-                min_row = min_row.min(row);
             },
             "D" => {
                 row += distance;
-                max_row = max_row.max(row);
             },
             "R" => {
                 col += distance;
-                max_col = max_col.max(col);
             },
             "L" => {
                 col -= distance;
-                min_col = min_col.min(col);
             },
             _ => panic!("Invalid"),
         }
-    }
-    let num_rows = (max_row - min_row) as usize + 1;
-    let num_cols = (max_col - min_col) as usize + 1;
-
-
-    let mut grid = vec![vec![b'.'; num_cols]; num_rows];
-    // lay down the loop on the grid
-    let (start_row, start_col) = (min_row.neg() as usize, min_col.neg() as usize);
-    let (mut row, mut col) = (start_row, start_col);
-    grid[row][col] = b'#';
-    for &(direction, distance, _) in instructions.iter() {
-        for _ in 0..distance {
-            match direction {
-                "U" => {
-                    row -= 1;
-                },
-                "D" => {
-                    row += 1;
-                },
-                "R" => {
-                    col += 1;
-                },
-                "L" => {
-                    col -= 1;
-                },
-                _ => panic!("Invalid"),
-            }
-            grid[row][col] = b'#';
-        }
-    }
-    // find cells that are inside the loop
-    let mut fill_grid = grid.clone();
-    for row_idx in 0..grid.len() {
-        let mut times_crossed = 0;
-        let mut from_south = false;
-        let mut from_north = false;
-        for col_idx in (0..grid[row_idx].len()).rev() {
-            match &grid[row_idx][col_idx] {
-                b'.' => {
-                    if from_north && from_south {
-                        from_north = false;
-                        from_south = false;
-                        times_crossed += 1;
-                    } else {
-                        from_north = false;
-                        from_south = false;
-                    }
-                    if times_crossed % 2 == 1 {
-                        fill_grid[row_idx][col_idx] = b'#';
-                    }
-                },
-                b'#' => {
-                    if row_idx > 0 {
-                        if grid[row_idx - 1][col_idx] == b'#' {
-                            from_north = true;
-                        }
-                    }
-                    if row_idx + 1 < grid.len() {
-                        if grid[row_idx + 1][col_idx] == b'#' {
-                            from_south = true;
-                        }
-                    }
-                },
-                _ => panic!("Invalid"),
-            }
-        }
+        double_area += shoelace((prev_col, prev_row), (col, row));
+        // pick's theorem
+        double_area += distance;
     }
 
-    let num_dug: usize = fill_grid.iter().map(|r| r.iter()).flatten().map(|&c| if c == b'#' {1} else {0}).sum();
-    println!("{}", num_dug);
+    println!("{}", double_area / 2 + 1);  // add one from start point
     println!("Total time: {:?}", start_time.elapsed());
 }
