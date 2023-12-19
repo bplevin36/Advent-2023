e60addc8d2135dc564f124b5fb03d3ab1868d02f
Ben Levin <bplevin36@gmail.com>
2023-12-19 13:07:31 +0000

    Add day 19 part 2; 276.43µs

--- src/bin/day_19.rs
+++ src/bin/day_19.rs
@@ -1,13 +1,13 @@
-use std::{collections::HashMap, cmp::Ordering, time::Instant};
+use std::{collections::HashMap, cmp::Ordering, time::Instant, ops::Range};
 
 use aoc2023::read_input;
-use nom::{IResult, bytes::complete::tag, character::complete::{u32 as parse_u32, alpha1, one_of}, sequence::{delimited, preceded, tuple}, branch::alt, combinator::map, multi::separated_list1};
+use nom::{IResult, bytes::complete::tag, character::complete::{u16 as parse_u16, alpha1, one_of}, sequence::{delimited, preceded, tuple}, branch::alt, combinator::map, multi::separated_list1};
 
 struct Part {
-    x: u32,
-    m: u32,
-    a: u32,
-    s: u32,
+    x: u16,
+    m: u16,
+    a: u16,
+    s: u16,
 }
 
 impl Part {
@@ -15,10 +15,10 @@ impl Part {
         let (i ,(x, m, a, s)) = delimited(
             tag("{"),
             tuple((
-                delimited(tag("x="), parse_u32, tag(",")),
-                delimited(tag("m="), parse_u32, tag(",")),
-                delimited(tag("a="), parse_u32, tag(",")),
-                preceded(tag("s="), parse_u32),
+                delimited(tag("x="), parse_u16, tag(",")),
+                delimited(tag("m="), parse_u16, tag(",")),
+                delimited(tag("a="), parse_u16, tag(",")),
+                preceded(tag("s="), parse_u16),
             )),
             tag("}")
         )(input)?;
@@ -34,7 +34,7 @@ impl Part {
 enum Instr<'i> {
     Accept,
     Reject,
-    Cmp{field: char, order: Ordering, value: u32, destination: &'i str},
+    Cmp{field: char, order: Ordering, value: u16, destination: &'i str},
     Redirect{workflow_name: &'i str},
 }
 
@@ -44,7 +44,7 @@ impl<'i> Instr<'i> {
             map(tag("A"), |_| Instr::Accept),
             map(tag("R"), |_| Instr::Reject),
             map(
-                tuple((one_of("xmas"), one_of("<>"), parse_u32, tag(":"), alpha1)),
+                tuple((one_of("xmas"), one_of("<>"), parse_u16, tag(":"), alpha1)),
                 |(field, cmp, value, _, destination)| Instr::Cmp {
                     field: field,
                     order: if cmp == '<' {Ordering::Less} else {Ordering::Greater},
@@ -78,39 +78,115 @@ fn parse_workflows_and_parts(input: &str) -> (HashMap<&str, Vec<Instr>>, Vec<Par
     (workflows, parts)
 }
 
-fn run_workflows(part: &Part, workflows: &HashMap<&str, Vec<Instr>>) -> u32 {
-    let mut workflow = &workflows["in"];
-    loop {
-        for instruction in workflow {
-            match instruction {
-                Instr::Accept => {
-                    return part.x + part.m + part.a + part.s;
-                },
-                Instr::Reject => return 0,
-                Instr::Cmp { field, order, value, destination } => {
-                    let our_field = match field {
-                        'x' => part.x,
-                        'm' => part.m,
-                        'a' => part.a,
-                        's' => part.s,
-                        _ => panic!("Invalid"),
-                    };
-                    if our_field.cmp(value) == *order {
-                        match *destination {
-                            "A" => return part.x + part.m + part.a + part.s,
-                            "R" => return 0,
-                            s => {
-                                workflow = &workflows[s];
-                                break;
+#[derive(Clone, Debug)]
+struct FieldRange {
+    x: Range<u16>,
+    m: Range<u16>,
+    a: Range<u16>,
+    s: Range<u16>,
+}
+
+impl FieldRange {
+    fn new() -> FieldRange {
+        FieldRange { x: 1..4001, m: 1..4001, a: 1..4001, s: 1..4001 }
+    }
+
+    fn split(mut self, field: char, at: u16) -> (FieldRange, Option<FieldRange>) {
+        let mut other = None;
+        match field {
+            'x' => {
+                if self.x.contains(&at) {
+                    let orig_x = self.x.clone();
+                    let mut other_field_range = self.clone();
+                    self.x = orig_x.start..at;
+                    other_field_range.x = at..orig_x.end;
+                    other = Some(other_field_range);
+                }
+            },
+            'm' => {
+                if self.m.contains(&at) {
+                    let orig_m = self.m.clone();
+                    let mut other_field_range = self.clone();
+                    self.m = orig_m.start..at;
+                    other_field_range.m = at..orig_m.end;
+                    other = Some(other_field_range);
+                }
+            },
+            'a' => {
+                if self.a.contains(&at) {
+                    let orig_a = self.a.clone();
+                    let mut other_field_range = self.clone();
+                    self.a = orig_a.start..at;
+                    other_field_range.a = at..orig_a.end;
+                    other = Some(other_field_range);
+                }
+            },
+            's' => {
+                if self.s.contains(&at) {
+                    let orig_s = self.s.clone();
+                    let mut other_field_range = self.clone();
+                    self.s = orig_s.start..at;
+                    other_field_range.s = at..orig_s.end;
+                    other = Some(other_field_range);
+                }
+            },
+            _ => panic!("Invalid"),
+        }
+        (self, other)
+    }
+
+    fn count_possible(&self) -> u64 {
+        let mut count = 1u64;
+        count *= (self.x.end).saturating_sub(self.x.start) as u64;
+        count *= (self.m.end).saturating_sub(self.m.start) as u64;
+        count *= (self.a.end).saturating_sub(self.a.start) as u64;
+        count *= (self.s.end).saturating_sub(self.s.start) as u64;
+        count
+    }
+}
+
+fn run_range(workflows: &HashMap<&str, Vec<Instr>>, mut range: FieldRange, start_instruction: (&str, usize), accepting_ranges: &mut Vec<FieldRange>) {
+    if start_instruction.0 == "A" {
+        accepting_ranges.push(range);
+        return;
+    } else if start_instruction.0 == "R" {
+        return;
+    }
+    for instruction in &workflows[start_instruction.0][start_instruction.1..] {
+        match instruction {
+            Instr::Accept => {
+                accepting_ranges.push(range);
+                return;
+            },
+            Instr::Reject => return,
+            Instr::Cmp { field, order, value, destination } => {
+                let at = match order {
+                    Ordering::Less => *value,
+                    Ordering::Greater => value + 1,
+                    _ => panic!("Invalid"),
+                };
+                match range.split(*field, at) {
+                    (smaller_range, Some(larger_range)) => {
+                        match order {
+                            Ordering::Less => {
+                                range = larger_range;
+                                run_range(workflows, smaller_range, (destination, 0), accepting_ranges);
                             },
+                            Ordering::Greater => {
+                                range = smaller_range;
+                                run_range(workflows, larger_range, (destination, 0), accepting_ranges);
+                            },
+                            _ => panic!("Invalid"),
                         }
+                    },
+                    (unchanged_range, None) => {
+                        range = unchanged_range;
                     }
-                },
-                Instr::Redirect { workflow_name } => {
-                    workflow = &workflows[workflow_name];
-                    break;
-                },
-            }
+                }
+            },
+            Instr::Redirect { workflow_name } => {
+                run_range(workflows, range.clone(), (workflow_name, 0), accepting_ranges);
+            },
         }
     }
 }
@@ -118,13 +194,13 @@ fn run_workflows(part: &Part, workflows: &HashMap<&str, Vec<Instr>>) -> u32 {
 pub fn main() {
     let start_time = Instant::now();
     let input = read_input("19");
-    let (workflows, parts) = parse_workflows_and_parts(&input);
+    let (workflows, _) = parse_workflows_and_parts(&input);
 
-    let mut sum = 0;
-    for part in parts.iter() {
-        sum += run_workflows(part, &workflows);
-    }
+    let mut accepting_ranges = Vec::new();
 
-    println!("{}", sum);
+    run_range(&workflows, FieldRange::new(), ("in", 0), &mut accepting_ranges);
+
+    let sum: u64 = accepting_ranges.iter().map(|r| r.count_possible()).sum();
+    println!("{:#?}", sum);
     println!("Total time: {:?}", start_time.elapsed());
 }
